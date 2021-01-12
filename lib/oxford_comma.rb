b2a0007acf828e3902694f45f672e1a03857c969
Kevin <kevhines@gmail.com>
2021-01-12 16:15:52 -0500

    commit tree 61663cab81cc111c2236d2633eec189258eff8f2

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+    stringed_array = ""
+    #if array.length == 1
+   #     stringed_array = array.join
+    #elsif array.length == 2
+    if array.length <= 2 
+        stringed_array = array.join(" and ")
+    elsif array.length > 2
+        counter = 0
+        while counter < array.length - 1 do
+            stringed_array << "#{array[counter]}, "
+            counter += 1
+        end
+        stringed_array << "and #{array.last}"
+    end
+    stringed_array
 end
